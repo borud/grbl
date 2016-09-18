c3c2e6bce9999335df320eb126f9784eccda4105
Bjorn Borud <bborud@gmail.com>
2016-09-18 23:13:31 +0200

    Flashing utility now asks for serial device

--- flash-firmware.sh
+++ flash-firmware.sh
@@ -3,5 +3,18 @@
 # Convenience script for flashing firmware to Arduino Uno on given
 # device.
 
-avrdude -pm328p -carduino -P /dev/cu.usbmodemfd141 -D -Uflash:w:grbl.hex
+if [ -z $1 ]; then
+    echo ""
+    echo "Usage: "
+    echo "  $0 <serial device>"
+    echo ""
+    echo "  Available serial devices:"
+    for a in `ls /dev/cu.*`; do
+	echo "    - $a";
+    done
+    echo ""
+    exit 1
+fi
+
+avrdude -pm328p -carduino -P $1 -D -Uflash:w:grbl.hex
 
